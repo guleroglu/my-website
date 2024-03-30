6ea3a46daf700b6d328b80635aa1437785b91949
Mustafa Guleroglu <must60yyy@gmail.com>
2024-03-30 23:29:18 +0300

    updated

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -17,14 +17,14 @@ export default function Header() {
 
       <nav>
         <ul
-          className={`
-         sm:flex sm:items-center sm:justify-center gap-4 max-sm:absolute left-6 top-5 max-sm:grid max-sm:grid-flow-row max-sm:gap-2 ${
-           !isOpen && "max-sm:hidden"
-         }`}
+          className={`  max-sm:p-2  max-sm:rounded-md max-sm:shadow-3xl
+      sm:flex sm:items-center sm:justify-center gap-4 max-sm:absolute left-6 top-5 max-sm:grid max-sm:grid-flow-row max-sm:gap-2   ${
+        !isOpen && "max-sm:hidden"
+      }`}
         >
           {mainMenu.map((item, index) => (
             <li
-              className="text-sm max-sm:text-xs font-medium text-[#D4D4D8] hover:underline transition-all tracking-normal"
+              className="text-sm max-sm:text-xs font-medium text-[#D4D4D8] hover:text-neutral-500 transition-all tracking-normal"
               key={index}
             >
               <NavLink to={item.path} activeClassName="active">
